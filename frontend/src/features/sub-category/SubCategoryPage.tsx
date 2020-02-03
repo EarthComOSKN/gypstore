4945c84d7bde26e3ee3ca5484d97fc8cf0de4515
Earth <mrearth25@gmail.com>
2020-02-03 15:00:33 +0700

    dsaf

--- frontend/src/features/sub-category/SubCategoryPage.tsx
+++ frontend/src/features/sub-category/SubCategoryPage.tsx
@@ -1,16 +1,17 @@
-import { useRouter } from "next/dist/client/router";
-import { Navbar } from "../../component/Navbar";
-import { Footer } from "../../component/Footer";
+import { useRouter } from 'next/dist/client/router'
+
+import { Footer } from '../../component/Footer'
+import { Navbar } from '../navigation/Navbar'
 
 export const SubCategoryPage = () => {
-  const router = useRouter();
-  if (!router) return null;
+  const router = useRouter()
+  if (!router) return null
 
-  console.log(router.query);
+  console.log(router.query)
   return (
     <div>
       <Navbar />
       <Footer />
     </div>
-  );
-};
+  )
+}
