5ca3a104b8c4e4a5f70930ffe85b0fda3fb50892
Earth <mrearth25@gmail.com>
2020-03-20 02:17:32 +0700

    now json

--- frontend/.next/static/chunks/styles.js
+++ frontend/.next/static/chunks/styles.js
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1584413836560");
+            injectCss(link, link.href.split("?")[0] + "?unix=1584645437584");
           });
       }
     }
@@ -66,7 +66,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1584413836166");
+            injectCss(link, link.href.split("?")[0] + "?unix=1584645437610");
           });
       }
     }
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,17 +1,17 @@
-import "../App.css";
-import "antd/dist/antd.min.css";
-import App, { Container } from "next/app";
-import Head from "next/head";
-import { ApolloProvider } from "@apollo/react-hooks";
-import ApolloClient from "../src/apollo-client";
+import '../App.css'
+import 'antd/dist/antd.min.css'
+import App, { Container } from 'next/app'
+import Head from 'next/head'
+import { ApolloProvider } from '@apollo/react-hooks'
+import ApolloClient from '../src/apollo-client'
 
 class MyApp extends App {
   constructor(props) {
-    super(props);
+    super(props)
   }
 
   render() {
-    const { Component, pageProps } = this.props;
+    const { Component, pageProps } = this.props
     return (
       <ApolloProvider client={ApolloClient}>
         <Head>></Head>
@@ -19,8 +19,8 @@ class MyApp extends App {
           <Component {...pageProps} />
         </Container>
       </ApolloProvider>
-    );
+    )
   }
 }
 
-export default MyApp;
+export default MyApp
